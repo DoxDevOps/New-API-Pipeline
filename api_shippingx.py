792808148211eac1b7ddc9f068b1ddbf2ff843b9
Jenkins <willmbowe@gmail.com>
2021-07-21 11:25:56 +0000

    testing

--- api_shippingx.py
+++ api_shippingx.py
@@ -1,3 +1,4 @@
+
 import requests
 import json
 import platform
@@ -36,7 +37,7 @@ for site_id in cluster['site']:
     # functionality for ping re-tries
     count = 0
 
-    while (count < 3):
+    while (count < 1):
 
         # lets check if the site is available
         param = '-n' if platform.system().lower()=='windows' else '-c'
@@ -53,11 +54,18 @@ for site_id in cluster['site']:
             # backing up application folder [API]
             # backup_script = "ssh " + site['username'] + "@" + site['ip_address'] + " 'cd /var/www && chmod 777 devops_api_backup.sh && ./devops_api_backup.sh'"
             # os.system(backup_script)
+            
+            ship_api = "rsync " + "-r $WORKSPACE/BHT-EMR-API " + site['username'] + "@" + site['ip_address'] + ":/var/www"
+            os.system(ship_api)
 
             # ship data to remote site
-            push_api = "cd /$WORKSPACE/BHT-EMR-API " + "git push --tags origin " + site['username'] + "@" + site[
-                'ip_address'] + ":/var/www/BHT-EMR-API"
-            os.system(push_api)
+            #api_add = "cd /$WORKSPACE/BHT-EMR-API " + "git remote add " + site['username'] + "@" + site[
+                #'ip_address'] + ":/var/www/BHT-EMR-API"
+            #os.system(api_add)
+            
+             # ship data to remote site
+            #push_api = "cd /$WORKSPACE/BHT-EMR-API " + "git push origin master"
+            #os.system(push_api)
 
             # run setup script
             run_api_script = "ssh " + site['username'] + "@" + site[
@@ -96,10 +104,10 @@ for site_id in cluster['site']:
           #      alert("http://ec2-52-14-138-182.us-east-2.compute.amazonaws.com:56733/v1/sms/send", params)
 
             # close the while loop
-          #  count = 3
- #else:
+            count = 1
+        else:
             # increment the count
-          #  count = count + 1
+            count = count + 1
 
             # make sure we are sending the alert at the last pint attempt
           #  if count == 3:
